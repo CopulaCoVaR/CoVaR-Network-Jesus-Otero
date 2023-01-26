dc2bcb5a9c96e8876ea69617528f70d53f9a97dc
msramirezgo <80695593+msramirezgo@users.noreply.github.com>
2023-01-26 16:17:15 -0500

    Ajustes generales de compatibilidad.

--- GRAFICACION_POST.R
+++ GRAFICACION_POST.R
@@ -37,12 +37,26 @@ if (0) {
   library(tidyquant)
   library(reshape2)
   library(igraph)
+  library(patchwork)
 } #Packages
-setwd("C:/Users/maico/OneDrive - Universidad Nacional de Colombia/BanRep/Value at Risk/Coffee")
+setwd("C:/Users/maico/OneDrive - Universidad Nacional de Colombia/BanRep/Value at Risk/CoVaR-Network-Jesus-Otero")
 wd = getwd()                                # Carpeta de trabajo adaptable al proyecto, se modifica automáticamente en cada computador.
+Resultados <<- paste0(wd,'/Resultados')     #<<<<<--- Capeta de resultados qué depende deldirectorio de trabajo. 
+n.ahead.connectedness=10                    # Step-ahead del GFEVD para la red. 
+
 # Carga de datos para cada país. 
-country=c("USA-Colombia", "USA-Brazil", "USA-Guatemala", "USA-Indonesia", "USA-Mexico", "USA-Uganda", "USA-Vietnam")[2]
-if (country=="USA-Colombia") {
+
+country=c("USA-Colombia", "USA-Brazil",
+          "USA-Guatemala", "USA-Indonesia",
+          "USA-Mexico", "USA-Uganda", 
+          "USA-Vietnam")
+
+plot.class=c("net","star","sphere", "circle")[1]
+if(plot.class=="net")plot.class=NULL
+
+for (kk in country) {
+  
+if (kk=="USA-Colombia") {
   #BigVAR.Optimo
   load(paste0(wd,"/Resultados/Colombia/R_files/BigVAR-Optimo-USA...Colombia-2023-01-10.R"), verbose=TRUE)
   # Copulas seleccionadas
@@ -57,7 +71,7 @@ if (country=="USA-Colombia") {
   load(paste0(wd,"/Resultados/Colombia/R_files/Dynamic_Network_USA...Colombia.R"),verbose=TRUE)
   
 }
-if (country=="USA-Brazil")   {
+if (kk=="USA-Brazil")   {
   #BigVAR.Optimo
   load(paste0(wd,"/Resultados/Brazil/R_files/BigVAR-Optimo-USA...Brazil-2023-01-11.R"), verbose=TRUE)
   # Copulas seleccionadas
@@ -72,7 +86,7 @@ if (country=="USA-Brazil")   {
   load(paste0(wd,"/Resultados/Brazil/R_files/Dynamic_Network_USA...Brazil.R"),verbose=TRUE)
   
 }
-if (country=="USA-Guatemala"){
+if (kk=="USA-Guatemala"){
   #BigVAR.Optimo
   load(paste0(wd,"/Resultados/Guatemala/R_files/BigVAR-Optimo-USA...Guatemala-2023-01-12.R"), verbose=TRUE)
   # Copulas seleccionadas
@@ -87,7 +101,7 @@ if (country=="USA-Guatemala"){
   load(paste0(wd,"/Resultados/Guatemala/R_files/Dynamic_Network_USA...Guatemala.R"),verbose=TRUE)
   
 }
-if (country=="USA-Indonesia"){
+if (kk=="USA-Indonesia"){
   #BigVAR.Optimo
   load(paste0(wd,"/Resultados/Indonesia/R_files/BigVAR-Optimo-USA...Indonesia-2023-01-12.R"), verbose=TRUE)
   # Copulas seleccionadas
@@ -102,7 +116,7 @@ if (country=="USA-Indonesia"){
   load(paste0(wd,"/Resultados/Indonesia/R_files/Dynamic_Network_USA...Indonesia.R"), verbose=TRUE)
   
 }
-if (country=="USA-Mexico")   {
+if (kk=="USA-Mexico")   {
   #BigVAR.Optimo
   load(paste0(wd,"/Resultados/Mexico/R_files/BigVAR-Optimo-USA...Mexico-2023-01-13.R"), verbose=TRUE)
   # Copulas seleccionadas
@@ -117,7 +131,7 @@ if (country=="USA-Mexico")   {
   load(paste0(wd,"/Resultados/Mexico/R_files/Dynamic_Network_USA...Mexico.R"),verbose=TRUE)
   
 }
-if (country=="USA-Uganda")   {
+if (kk=="USA-Uganda")   {
   #BigVAR.Optimo
   load(paste0(wd,"/Resultados/Uganda/R_files/BigVAR-Optimo-USA...Uganda-2023-01-13.R"), verbose=TRUE)
   # Copulas seleccionadas
@@ -132,7 +146,7 @@ if (country=="USA-Uganda")   {
   load(paste0(wd,"/Resultados/Uganda/R_files/Dynamic_Network_USA...Uganda.R"),verbose=TRUE)
   
 }
-if (country=="USA-Vietnam")  {
+if (kk=="USA-Vietnam")  {
   #BigVAR.Optimo
   load(paste0(wd,"/Resultados/Vietnam/R_files/BigVAR-Optimo-USA...Vietnam-2023-01-08.R"), verbose=TRUE)
   # Copulas seleccionadas
@@ -147,22 +161,19 @@ if (country=="USA-Vietnam")  {
   #load(,verbose=TRUE)
   
 }
-#Corrección de nombres
-if(1){
-  Series           = c("USA...Colombia",        "USA...Brazil",          "USA...Guatemala",       "USA...Indonesia",      
-                       "USA...Mexico",          "USA...Uganda",            "USA...Vietnam",       "Germany...Colombia",
-                       "Germany...Brazil",      "Germany...El.Salvador", "Germany...Guatemala",   "France...Colombia",
-                       "France...Brazil",       "France...Cote.dIvoire", "France...Indonesia",    "France...Uganda", 
-                       "France...Vietnam")
-  Series.corrected = c("USA-Colombia",        "USA-Brazil",          "USA-Guatemala",       "USA-Indonesia",      
-                       "USA-Mexico",          "USA-Uganda",            "USA-Vietnam",       "Germany-Colombia",
-                       "Germany-Brazil",      "Germany-El.Salvador", "Germany-Guatemala",   "France-Colombia",
-                       "France-Brazil",       "France-Cote.dIvoire", "France-Indonesia",    "France-Uganda", 
-                       "France-Vietnam")
+
+
+# Network -----------------------------------------------------------------
+if (1) {
+  Network.plot.data=Connectedness(GFEVD     =GFEVD.res, Title=paste0("Net Pairwise directional connectedness","\n" ,"Stressed serie: ",kk), 
+                                  node.size =1.5,   node.label.size=0.7, Q1='95%', Q2='90%', Q3='85%', 
+                                  arrow.size=0.6,  layout=plot.class, n.ahead = n.ahead.connectedness, 
+                                  edge.width.scale=2, pdf.name.serie=kk) 
+  graphics.off()
 }
 
 # CoVaR plot --------------------------------------------------------------
-if(0){
+if(0)  {
   # Unión de datos en la misma lista.
   CoVaR_DATA=CoVaR_data
   plot.class = c('Up', 'Down', 'Both')[1]
@@ -174,11 +185,13 @@ if(0){
     if(plot.class=='Down')range=c(min(CoVaR_DATA$CoVaR[,i]),0)
     if(plot.class=='Both')range=c(min(CoVaR_DATA$CoVaR[,i]),max(CoVaR_DATA$CoVaRUp[,i]))
     pdf(file = paste0(Resultados,'/Graficas_CoVaR','_',i,'.pdf'), onefile=FALSE)
-    print(plot.xts(if (plot.class=='Down'|plot.class=='Both')CoVaR_DATA$CoVaR[,i]else CoVaR_DATA$CoVaRUp[,i],
-               type="l",col="red", grid.col = NA, ylim=range, xlab="Time", ylab="", main=i,
-               #main.timespan=FALSE, 
-               lwd=1,format.labels="%Y", major.ticks = 'years', yaxis.left=TRUE, 
-               yaxis.right=TRUE, lty='solid'))
+    print(plotxts(if (plot.class=='Down'|plot.class=='Both')
+    { CoVaR_DATA$CoVaR[,i]} else{
+      CoVaR_DATA$CoVaRUp[,i] },
+    type="l",col="red", grid.col = NA, 
+    ylim=range, xlab="Time", ylab="", lwd=1, main=i,format.labels="%Y", major.ticks = 'years', 
+    yaxis.left=TRUE, yaxis.right=TRUE, lty='solid', cex=0.5, cex.axis=0.8)) 
+    #, cex.main=2.5, cex.sub=1.5, cex.axis=1.5
     if (plot.class=='Down'|plot.class=='Both') print(lines(CoVaR_DATA$VaR[,i],         col="black", lwd=1,lty='dashed'))
     if (plot.class=='Both')                    print(lines(CoVaR_DATA$CoVaRUp[,i],     col="red",   lwd=1,lty='solid'))
     if (plot.class=='Up'|plot.class=='Both')   print(lines(CoVaR_DATA$VaRUp[,i],       col="black", lwd=1,lty='dashed'))
@@ -190,12 +203,12 @@ if(0){
   }
 }
 
-#Network Dynamic
-if(0) {
+# Network Dynamic 
+if(0)  {
+  theme=c("classic", "grey")[2]
   # Preparación
   for(i in 1:ncol(Dynamic_Network$From.Degree.Dynamic)){
     Serie.1 = colnames(Dynamic_Network$From.Degree.Dynamic[,i])
-    if (Serie.1==Series[i]) Serie.1=Series.corrected[i]
     # Datos
     to        = Dynamic_Network$To.Degree.Dynamic[,i]
     from      = Dynamic_Network$From.Degree.Dynamic[,i]
@@ -207,30 +220,33 @@ if(0) {
     
     # Graficación
     if(theme=="classic"){
-      to.plot   = ggplot(data=data.plot, mapping=aes(x=Time, y=To))  +geom_line(colour="steelblue")+theme_classic() + labs(x = element_blank(), y = "To") 
-      from.plot = ggplot(data=data.plot, mapping=aes(x=Time, y=From))+geom_line(colour="steelblue")+theme_classic() + labs(x = element_blank(), y = "From")
-      net.plot  = ggplot(data=data.plot, mapping=aes(x=Time, y=Net)) +geom_line(colour="steelblue")+theme_classic() + labs(x = element_blank(), y = "Net")
-      plot      = to.plot + from.plot + net.plot + plot_layout(ncol=1) + plot_annotation(title = paste("Dynamic total directional connectedness of", country),
-                                                                                         subtitle = paste0("Stressed series: ",Serie.1))
+      to.plot   = ggplot(data=data.plot, mapping=aes(x=Time, y=To))    +geom_line(colour="steelblue")+theme_classic() + labs(x = element_blank(), y = "To")  +scale_x_date(expand=c(0,0), limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"]))
+      from.plot = ggplot(data=data.plot, mapping=aes(x=Time, y=From))  +geom_line(colour="steelblue")+theme_classic() + labs(x = element_blank(), y = "From")+scale_x_date(expand=c(0,0), limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"]))
+      net.plot  = ggplot(data=data.plot, mapping=aes(x=Time, y=Net))   +geom_line(colour="steelblue")+theme_classic() + labs(x = element_blank(), y = "Net") +scale_x_date(expand=c(0,0), limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"]))
+      plot      = to.plot + from.plot + net.plot + plot_layout(ncol=1) 
+
       x11()
-      print(plot) 
-      #+  scale_x_continuous(limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"]),expand = c(0, 0))
-    }    
+      print(plot + plot_annotation(title = paste("Dynamic total directional connectedness of", Serie.1),
+                                   subtitle = paste0("Stressed series: ",country)))
+      }    
     if(theme=="grey")   {
-      to.plot   = ggplot(data=data.plot, mapping=aes(x=Time, y=To))  +geom_line(colour="steelblue")+theme_gray() + labs(x = element_blank(), y = "To") 
-      from.plot = ggplot(data=data.plot, mapping=aes(x=Time, y=From))+geom_line(colour="steelblue")+theme_gray() + labs(x = element_blank(), y = "From")
-      net.plot  = ggplot(data=data.plot, mapping=aes(x=Time, y=Net)) +geom_line(colour="steelblue")+theme_gray() + labs(x = element_blank(), y = "Net")
-      plot      = to.plot + from.plot + net.plot + plot_layout(ncol=1) + plot_annotation(title = paste("Dynamic total directional connectedness of", country),
-                                                                                         subtitle = paste0("Stressed series: ",Serie.1))
-      x11()
-      print(plot) 
-      #+  scale_x_continuous(limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"]),expand = c(0, 0))
+      to.plot   = ggplot(data=data.plot, mapping=aes(x=Time, y=To))    +geom_line(colour="steelblue")+theme_gray() + labs(x = element_blank(), y = "To")  +scale_x_date(expand=c(0,0), limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"])+2)
+      from.plot = ggplot(data=data.plot, mapping=aes(x=Time, y=From))  +geom_line(colour="steelblue")+theme_gray() + labs(x = element_blank(), y = "From")+scale_x_date(expand=c(0,0), limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"])+2)
+      net.plot  = ggplot(data=data.plot, mapping=aes(x=Time, y=Net))   +geom_line(colour="steelblue")+theme_gray() + labs(x = element_blank(), y = "Net") +scale_x_date(expand=c(0,0), limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"])+2)
+      plot      = to.plot + from.plot + net.plot + plot_layout(ncol=1) 
+      
+      pdf(file = paste0(Resultados,'/Graficas_CoVaR','_',kk,"_",Serie.1,'.pdf'), onefile=FALSE)
+      print(plot + plot_annotation(title = paste("Dynamic total directional connectedness of", Serie.1),
+                                   subtitle = paste0("Stressed series: ",country)))
+      
+      
     }
+    graphics.off()
   }
-  
 } 
+
 #CoVaR plot
-if (0) {
+if(0)  {
   for (i in 1:ncol(CoVaR_data$CoVaRUp)) {
     CoVaRUp   = CoVaR_data$CoVaRUp[,i]
     VaRUp     = CoVaR_data$VaRUp[,i]
@@ -238,12 +254,15 @@ if (0) {
     data.plot = fortify(data.xts)
     colnames(data.plot) = c("Time","CoVaR", "VaR")
     data.plot[,"Time"]=as.Date(data.plot[,"Time"])
-    plot=ggplot(title="") + theme_gray()+ geom_line(data = data.plot, aes(x = Time, y = CoVaR), color = "red") +
+    plot=ggplot(title="") + theme_classic()+ geom_line(data = data.plot, aes(x = Time, y = CoVaR), color = "red") +
       geom_line(data = data.plot, aes(x = Time, y = VaR), color = "black") +
-      xlab(element_blank()) +
-      ylab(element_blank())
+      xlab("Time") +
+      ylab(element_blank())+
+      ggtitle(paste0("CoVaR and VaR for ", colnames(CoVaR_data$CoVaRUp)[i]))+
+      scale_x_date(expand=c(0,0), limits=c(data.plot[1,"Time"],data.plot[nrow(data.plot),"Time"]))
     x11()
     print(plot)
   }
 }
 
+}
